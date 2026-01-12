37e94ce7a468a43e13b2a2b99a3b15077470b877
Florian Hensel <florinator543@gmail.com>
2026-01-12 15:42:11 +0100

    Add map1.ber file and update so_long.c with map validation comments

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: flhensel <flhensel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:09:08 by flhensel          #+#    #+#             */
-/*   Updated: 2026/01/12 15:31:43 by flhensel         ###   ########.fr       */
+/*   Updated: 2026/01/12 15:40:55 by flhensel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,7 @@ int main(int ac, char *av[])
 {
 	// Check if number of Arguments is right
 	// Check if Argument is valid
+	// Read Map
+	// Check if Map is valid
+	
 }
