63f340a5fa154ecc228f26e0a3797a5f582225d2
Florian Hensel <florinator543@gmail.com>
2026-01-12 15:32:07 +0100

    Add main function skeleton with argument checks

--- so_long.c
+++ so_long.c
@@ -6,8 +6,14 @@
 /*   By: flhensel <flhensel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:09:08 by flhensel          #+#    #+#             */
-/*   Updated: 2026/01/12 15:10:09 by flhensel         ###   ########.fr       */
+/*   Updated: 2026/01/12 15:31:43 by flhensel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int main(int ac, char *av[])
+{
+	// Check if number of Arguments is right
+	// Check if Argument is valid
+}
